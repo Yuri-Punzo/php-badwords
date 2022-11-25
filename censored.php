a1c263b80f6e60ede8afe79fdd3d2484e5b316a0
Yuri-Punzo <yuripunzo@hotmail.it>
2022-11-25 16:38:32 +0100

    final commit

---
+++ censored.php
@@ -0,0 +1,13 @@
+<?php
+/* var_dump($array); */
+
+$frase = $_GET["frase"];
+
+$badword = $_GET["badword"];
+
+echo "Frase: " . $frase . " Lunghezza: " . strlen($frase) . "<br>";
+
+$newFrase = str_replace($badword, "***", $frase);
+
+echo "Frase censurata: " . $newFrase . " Lunghezza: " . strlen($newFrase)
+?>
